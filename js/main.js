70e406c768a59e828190530228c797ca50a4b61d
Victor Taborda <tabordavictorjose@gmail.com>
2022-07-31 12:59:02 -0300

    se agrega DOM para vinculacion de html y js

--- js/main.js
+++ js/main.js
@@ -1,113 +1,54 @@
-
-let nombre;
 class producto {
-    constructor(id, nombre, precio) {
+    constructor(id, nombre, precio, descripcion, img) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
-    }
-    valor() { alert("El valor del producto seleccionado es $ " + this.precio) }
-}
-const producto1 = new producto(1, "Paleta Bullpadel Naranja", 35000)
-const producto2 = new producto(2, "Paleta Bullpadel Azul", 65000)
-const producto3 = new producto(3, "Paleta Bullpadel Negra", 135000)
-const producto4 = new producto(4, "Grip Generico", 200)
-const producto5 = new producto(5, "Grip Intermedio", 420)
-const producto6 = new producto(6, "Grip Profesional", 900)
-const producto7 = new producto(7, "Grip Profesional", 100)
-const producto8 = new producto(8, "Tubo x 3 Pelotas Head", 3500)
-const productos = [];
-const cliente = () => {
-    do {
-        nombre = prompt("Hola, Gracias por visitar nuestra tienda en punto de oro, porfavor indicanos tu Nombre.");
-    }
-    while (nombre === "" || !isNaN(nombre))
-    return nombre
-}
-
-let agregarCarrito = () => {
-    let seleccionCliente = Number(prompt("Hola " + nombre + " bienvenido/a a nuestra tienda, selecciona con NUMEROS el producto que desea comprar\n\n 1- Paleta bullpadel  Naranja. \n 2- Paleta Bullpadel Azul. \n 3- Paleta Bullpadel Negra. \n 4- Grip Generico.\n 5- Grip Intermedio. \n 6- Grip Profesional.\n 7- Grip Profesional. \n 8- Tubo x 3 Pelotas Head "))
-    while (seleccionCliente != "salir") {
-        switch (seleccionCliente) {
-            case producto1.id:
-                producto1.valor()
-                productos.push(producto1)
-                seleccionCliente = "salir";
-                break
-            case producto2.id:
-                producto2.valor()
-                productos.push(producto2)
-                seleccionCliente = "salir";
-                break
-            case producto3.id:
-                producto3.valor()
-                productos.push(producto3)
-                seleccionCliente = "salir";
-                break
-            case producto4.id:
-                producto4.valor()
-                productos.push(producto4)
-                seleccionCliente = "salir";
-                break
-            case producto5.id:
-                producto5.valor()
-                productos.push(producto5)
-                seleccionCliente = "salir";
-                break
-            case producto6.id:
-                producto6.valor()
-                productos.push(producto6)
-                seleccionCliente = "salir";
-                break
-            case producto7.id:
-                producto7.valor()
-                productos.push(producto7)
-                seleccionCliente = "salir";
-                break
-            case producto8.id:
-                producto8.valor()
-                productos.push(producto8)
-                seleccionCliente = "salir";
-                break
-            default:
-                seleccionCliente = Number(prompt("por favor, ingresa nuevamente el NUMERO del producto que deseas comprar \n\n 1- Paleta bullpadel  Naranja. \n 2- Paleta Bullpadel Azul. \n 3- Paleta Bullpadel Negra. \n 4- Grip Generico.\n 5- Grip Intermedio. \n 6- Grip Profesional.\n 7- Grip Profesional. \n 8- Tubo x 3 Pelotas Head \n"));
-
-
-        }
-    }
-    if (confirm("¿Queres agregar otro producto a la compra?")) {
-        agregarCarrito();
-    }
-    else {
-        alert("La suma de tu compra es $ " + totalcarrito())
+        this.descripcion = descripcion;
+        this.img = img;
     }
 }
 
-const totalcarrito = () => {
-    let sumaCarro = 0;
-    productos.forEach((producto) => {
-        sumaCarro += producto.precio
+const producto1 = new producto(1, "PALA BULLPADEL HACK JR 22", 36000, "La nueva Pala Hack 02 JR es la pala oficial de Paquito Navarro adaptada a jugadores junior.", `./img/producto0.jpg`)
+const producto2 = new producto(2, "PALA BULLPADEL K3 AVANT 21", 65000, "Pala Bullpadel K3 Avant colec. 2021 , máximo control y alto rendimiento.  para jugadores avanzados.", `./img/producto1.jpg`)
+const producto3 = new producto(3, "PALA BULLPADEL ELITE W", 135000, "La nueva pala ELITE de Bullpadel es la pala elegida por nuestra jugadora Gemma Triay.", `./img/producto2.jpg`)
+const producto4 = new producto(4, "GRIPS Bullpadel GR-1210", 200, "Grips de reemplazo de máxima durabilidad.Blanco y Negro", `./img/producto3.jpg`)
+const producto5 = new producto(5, "GRIP Bullpadel CUSTOM WEIGHT", 420, "Pack de 2 unidades de pesos para añadir al mango de la pala.", `./img/producto4.jpg`)
+const producto6 = new producto(6, "OVERGRIPS Bullpadel GB-1202", 900, "Sistema FixoGrip ofrece seguridad, agarre y confort óptimos", `./img/producto5.jpg`)
+const producto7 = new producto(7, "PELOTAS Bullpadel GOLD", 600, "Pack de 3 pelotas de gran velocidad, calidad y durabilidad.", `./img/producto6.jpg`)
+const producto8 = new producto(8, "PELOTAS Bullpadel BP FIP NEXT PRO", 900, "Pack de pelotas NEXT PRO para competiciones y torneos.", `./img/producto7.jpg`)
 
-    })
-    return sumaCarro
+
+let cardsPaletas = [producto1, producto2, producto3];
+let cardsGrips = [producto4, producto5, producto6];
+let cardsPelotas = [producto7, producto8]
+let cardsContainer = [cardsPaletas, cardsGrips, cardsPelotas];
+
+const cardPala = document.querySelector(".container_pala");
+const cardGrip = document.querySelector(".cointeiner_grip");
+const cardPelota = document.querySelector(".cointeiner_bolas");
+const containers = [cardPala, cardGrip, cardPelota];
+for (i = 0; i < cardsContainer.length; i++) {
+
+    let htmlCode = "";
+    for (cards in cardsContainer[i]) {
+        let card = cardsContainer[i][cards];
+        let img = card["img"];
+        let nombre = card["nombre"];
+        let descripcion = card["descripcion"];
+        let precio = card["precio"];
+        let id = card["id"];
+        htmlCode += `
+        <div class="card m-4" style="width: 15rem;">
+        <img src="${img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${nombre}</h5>
+        <p class="card-text">${descripcion}</p>
+        <h1>$${precio}<h1>
+        <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>`;
+    };
+
+    containers[i].innerHTML = htmlCode;
 }
 
-const eliminarProducto = () => {
-    productoEliminar = prompt("Si desea eliminar un productos por favor indiquelo con el numero. \n\n 1- Paleta bullpadel  Naranja. \n 2- Paleta Bullpadel Azul. \n 3- Paleta Bullpadel Negra. \n 4- Grip Generico.\n 5- Grip Intermedio. \n 6- Grip Profesional.\n 7- Grip Profesional. \n 8- Tubo x 3 Pelotas Head \n ")
-
-    let num = 0;
-    productos.forEach((producto) => {
-
-        if (producto.id == productoEliminar) {
-            productos.splice(num, 1);
-            num += 1;
-        } else {
-            num += 1;
-        }
-        alert ("Su total actualizado es de $ " + totalcarrito())
-    })
-}
-
-cliente();
-agregarCarrito();
-eliminarProducto();
